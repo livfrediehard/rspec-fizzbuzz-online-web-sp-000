56e9a153cd418237bb89026422383cf6a0997212
Brian Palmer <bpalmer710@gmail.com>
2021-05-26 16:07:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,14 @@
 # See README.md for instructions on how to do this	# See README.md for instructions on how to do this
 
 def fizzbuzz
-  if fizzbuzz == 
+  if fizzbuzz == 3
+    puts "Fizz"
+  elsif fizzbuzz == 5
+    puts "Buzz"
+  elsif fizzbuzz == 15
+    puts "FizzBuzz"
+  else
+    puts "nil"
 end
 
 
